872edb211fc98704432521c080f4e40b6c694a81
Timothy Rolen <timothy.rolen@gmail.com>
2023-02-06 19:21:00 -0500

    Add transaction detail

--- penny_doubled/penny_doubled.py
+++ penny_doubled/penny_doubled.py
@@ -13,8 +13,19 @@ def get_input():
     return result
 
 
+def show_transactions(n):
+    balance = .01
+    for i in range(n):
+        print('Day', i + 1, 'balance:', balance)
+        balance *= 2
+
+
 def main():
     num_days = get_input()
+    print('Daily Transactions')
+    print('------------------')
+    show_transactions(num_days)
+    print('------------------')
     result = .01 * (2 ** (num_days - 1))
     print('Doubling one penny for ' + str(num_days) + ' days -> ' + str(result))
 
